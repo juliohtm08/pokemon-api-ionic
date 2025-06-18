b0672111621802af1c8a646dd717b645a91ce17a
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-18 18:34:12 -0300

    Implementando barra de pesquisa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
+import { Subject, takeUntil } from 'rxjs';
 import { IPokemonDatas } from '../models/interfaces/pokemon.interface';
 import { PokeAPIService } from './../modules/pokemon/service/poke-api.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [],
   standalone: false,
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject();
+
   initialPokemonName = 'ditto';
   pokemonDatas!: IPokemonDatas;
 
@@ -18,13 +21,27 @@ export class Tab1Page implements OnInit {
     this.getPokemonDatas(this.initialPokemonName);
   }
 
+  // busca o pokemon na API
   getPokemonDatas(pokemonName: string) {
-    this.pokeAPIService.getPokemonDatas(pokemonName).subscribe({
-      next: (response) => {
-        response && (this.pokemonDatas = response);
-        console.log(this.pokemonDatas);
-      },
-      error: (err) => console.log(err),
-    });
+    this.pokeAPIService
+      .getPokemonDatas(pokemonName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.pokemonDatas = response);
+          console.log(this.pokemonDatas);
+        },
+        error: (err) => console.log(err),
+      });
+  }
+
+  onSubmit(): void {
+    this.getPokemonDatas(this.initialPokemonName);
+    this.initialPokemonName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
